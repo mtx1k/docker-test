d9dc89aac64efa438143ec40d1516893c93adeef
Vitalii <vitaliy.shekhovtsov@gmail.com>
2024-11-07 11:06:46 +0100

    initial

--- src/main/java/dci/docker_test/AppController.java
+++ src/main/java/dci/docker_test/AppController.java
@@ -1,4 +1,12 @@
 package dci.docker_test;
 
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
 public class AppController {
+    @GetMapping("/")
+    public String index(Model model) {
+        model.addAttribute("name", "Joe");
+        return "index";
+    }
 }
